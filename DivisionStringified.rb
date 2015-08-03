5d203f2331681494d017dfae86b58fa24c5b219e
cho2942 <cho_4528@naver.com>
2015-08-03 18:11:17 +0900

    Create DivisionStringified.rb

---
+++ DivisionStringified.rb
@@ -0,0 +1,11 @@
+def DivisionStringified(num1,num2)
+
+  # code goes here\
+  num1=num1*1.000
+  return (num1/num2).round
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+DivisionStringified(STDIN.gets) 
